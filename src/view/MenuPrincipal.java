07ddb3359aef7c6c3caeab0194ed4a495955f828
murillosm <murillosm@hotmail.com>
2022-12-12 00:46:37 -0300

    implementação com Correções

--- src/view/MenuPrincipal.java
+++ src/view/MenuPrincipal.java
@@ -27,21 +27,21 @@ public class MenuPrincipal extends JFrame {
 	static JDesktopPane desktopPane;
 	MenuPrincipal menuPrincipal;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MenuPrincipal frame = new MenuPrincipal();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+//	/**
+//	 * Launch the application.
+//	 */
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					MenuPrincipal frame = new MenuPrincipal();
+//					frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
 
 	/**
 	 * Create the frame.
